0362deb96b0a22b41798b56f3789ea37c00bb3cf
Tony Kelanen <tony.kelanen@student.careeria.fi>
2025-02-13 11:36:49 +0200

    Customer controller to get all and by id

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -11,13 +11,38 @@ namespace NorthwindRestApi.Controllers
         NorthwindOriginalContext db = new NorthwindOriginalContext();
         // or NorthwindOriginalContext db =  new();
 
+
         //Hakee kaikki asiakkaat
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var customers = db.Customers.ToList();
-            return Ok(customers);
+            try
+            {
+                var customers = db.Customers.ToList();
+                return Ok(customers);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Something went wrong! " + e.InnerException);
+            }
         }
 
+
+        //Hakee asiakkaan id n mukaan 
+        [HttpGet("{id}")]
+        // or [Route("{id}")]
+        public ActionResult GetOneCustomersById(string id)
+        {
+            var customer = db.Customers.Find(id);
+            if (customer != null)
+            {
+                return Ok(customer);
+            }
+            else
+            {
+                //return BadRequest("Customer for that id " + id + "not found");
+                return BadRequest($"Customer for id {id} not found"); // String interpolation
+            }
+        }
     }
 }
